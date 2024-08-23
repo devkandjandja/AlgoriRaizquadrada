16d1363ece76191de4fdb0e16478ceb5585f0633
Laurindo Firmino <Laurindo kandjandja>
2024-08-23 05:12:39 +0100

    TreinandoAlgoritios

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿double a, b, c, d;
+double x1, x2;
+Console.WriteLine("Digite o primeiro numero: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o segundo  numero: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o segundo numero: ");
+c =  Convert.ToInt32(Console.ReadLine());
+
+d = Math.Sqrt(b) - 4 * a *c;
+
+if(d > 0)
+{
+    x1 = (-b - Math.Sqrt(d) / (2 * a));
+    x2  = (-b + Math.Sqrt(d) / (2 * a));
+    Console.WriteLine("X1 = ", x1, "X2 = ", x2);
+}
+else{
+    if(d == 0)
+    {
+        x1  = (-b / (2 * a));
+        x2 = x1;
+        Console.WriteLine("X1 = ", x1, " X2= ", x2);
+    }
+    Console.WriteLine("Impossivel!");
+}
+
